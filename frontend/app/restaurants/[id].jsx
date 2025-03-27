b66b64baa01b081f993abb405aefc498772f1f73
ThanhHigh <ducthanhtre2004@gmail.com>
2025-03-27 15:44:42 +0700

    navigate home tab bar

---
+++ frontend/app/restaurants/[id].jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SingleRestaurant() {
+  return (
+    <View>
+      <Text>SingleRestaurant</Text>
+    </View>
+  )
+}
