f2e9e00a3981e50744ee29c3e004b4b9371721e7
TranHauNam <haunam3112@gmail.com>
2025-05-12 16:31:44 +0700

    admin: update giao diện chỉnh sửa nhà hàng

--- frontend/services/api.jsx
+++ frontend/services/api.jsx
@@ -1,5 +1,7 @@
+import { API_URL } from './config';
+
 export const API_CONFIG = {
-    BASE_URL: "http://192.168.0.100:5001",
+    BASE_URL: API_URL,
     headers: {
       accept: "application/json",
     },
