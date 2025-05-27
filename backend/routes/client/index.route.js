9c5b6f4c29fdee37878db43be439e7e35ac74969
TranHauNam <haunam3112@gmail.com>
2025-05-27 18:14:54 +0700

    fix api noti

--- backend/routes/client/index.route.js
+++ backend/routes/client/index.route.js
@@ -1,7 +1,9 @@
 const authRoutes = require('./auth.route');
 const restaurantRoutes = require('./restaurent.route')
+const notificationRoutes = require('./notification.route')
 
 module.exports = (app) => {
     app.use('/api/auth', authRoutes);
     app.use('/api/restaurant', restaurantRoutes);
+    app.use('/api/notification', notificationRoutes);
 }
